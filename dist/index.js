d1484d09892a0f107cfaf6b3eb079e54e825706d
Ozymandias <tomas.rav@gmail.com>
2019-06-29 06:57:16 +0300

    - Fixed unintended code splitting - Updated package.json

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,118 @@
-"use strict";function _interopDefault(e){return e&&"object"==typeof e&&"default"in e?e.default:e}var rollupPluginutils=require("rollup-pluginutils"),matched=_interopDefault(require("matched")),name="entry-glob",entry="\0rollup-plugin-entry-glob:single-entry",suppressed="\0rollup-plugin-entry-glob:suppressed",sortArray=function(e){var n=["js","mjs","jsx","es","es6","ts","tsx","json"].reverse(),r=["html","htm","php","css","scss","sass","less","png","jpg","jpeg","gif","svg"];return e.sort(function(e,t){var s=e.split(".").pop(),u=t.split(".").pop();return-n.indexOf(s)+r.indexOf(s)-(-n.indexOf(u)+r.indexOf(u))})};function entryGlob(e){var n=e.fileName?"\0"+e.fileName:entry,r=e.include||e.exclude?rollupPluginutils.createFilter(e.include,e.exclude):rollupPluginutils.createFilter("**/*"),t=[],s=[],u=function(e){return"export * from "+JSON.stringify(e)+";"},l=function(e){return"import "+JSON.stringify(e)+";"};return e.exports=void 0===e.exports||!0===e.exports,{name:name,options:function(e){var u=[n,suppressed],l={};if(e.manualChunks)for(var i=function(){var e=p[o],n=e[0],r=e[1];l[n]=[],matched.sync(r,{realpath:!0}).forEach(function(e){l[n].push(e),s.push(e)})},o=0,p=Object.entries(e.manualChunks);o<p.length;o+=1)i();e.input&&e.input!==n&&matched.sync(e.input,{realpath:!0}).forEach(function(e){s.includes(e)||(r(e)&&t.push(e),u.push(e))}),e.manualChunks=l,e.input=sortArray(u)},resolveId:function(e){return e===n?n:e===suppressed?suppressed:null},load:async function(r){return r===n?t.length?Promise.resolve(e.exports?t.map(u).join("\n"):t.map(l).join("\n")):Promise.resolve(""):r===suppressed?s.length?Promise.resolve(s.map(u).join("\n")):Promise.resolve(""):void 0},generateBundle:async function(e,r){var s=n.replace("\0","_")+".js";r[s].fileName=n.replace("\0","")+".js",t.length||delete r[s],/\(function\s*\(\)\s*\{\s*\}\)\(\);/g.test(r[s].code)&&delete r[s],delete r[suppressed.replace("\0","_")+".js"];for(var u=0,l=Object.entries(r);u<l.length;u+=1){var i=l[u],o=i[0],p=i[1];t.includes(p.facadeModuleId)&&p.isEntry&&delete r[o]}}}}module.exports=entryGlob;
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var rollupPluginutils = require('rollup-pluginutils');
+var matched = _interopDefault(require('matched'));
+
+var name = 'entry-glob';
+var entry = '\0rollup-plugin-entry-glob:single-entry';
+var suppressed = '\0rollup-plugin-entry-glob:suppressed';
+
+
+var sortArray = function (arr) {
+	var scriptOrder = ['js', 'mjs', 'jsx', 'es', 'es6', 'ts', 'tsx', 'json'].reverse();
+	var assetOrder = ['html', 'htm', 'php', 'css', 'scss', 'sass', 'less', 'png', 'jpg', 'jpeg', 'gif', 'svg'];
+	return arr.sort(function (a, b) {
+		var aExt = a.split('.').pop();
+		var bExt = b.split('.').pop();
+		return (-scriptOrder.indexOf(aExt) + assetOrder.indexOf(aExt)) - (-scriptOrder.indexOf(bExt) + assetOrder.indexOf(bExt));
+	});
+};
+
+
+function entryGlob(options) {
+	var module = options.fileName
+		? ("\u0000" + (options.fileName))
+		: entry;
+	var filter = options.include || options.exclude
+		? rollupPluginutils.createFilter(options.include, options.exclude)
+		: rollupPluginutils.createFilter('**/*');
+	var imports = [];
+	var chunked = [];
+	var exporter = function (path) { return ("export * from " + (JSON.stringify(path)) + ";"); };
+	var importer = function (path) { return ("import " + (JSON.stringify(path)) + ";"); };
+	options.exports = options.exports === undefined || options.exports === true
+		? true
+		: false;
+
+	return {
+		name: name,
+
+		options: function options(config) {
+			var ret = [];
+			var chunks = {};
+			if (config.manualChunks) {
+				var loop = function () {
+					var ref = list[i];
+					var chunk = ref[0];
+					var patterns = ref[1];
+
+					chunks[chunk] = [];
+					matched.sync(patterns, { realpath: true })
+						.forEach(function (path) {
+							chunks[chunk].push(path);
+							chunked.push(path);
+						});
+				};
+
+				for (var i = 0, list = Object.entries(config.manualChunks); i < list.length; i += 1) loop();
+			}
+			if (config.input && config.input !== module) {
+				matched.sync(config.input, { realpath: true })
+					.forEach(function (path) {
+						if (!chunked.includes(path)) {
+							if (filter(path)) { imports.push(path); }
+							else { ret.push(path); }
+						}
+					});
+			}
+			config.manualChunks = chunks;
+			config.input = Object.entries(chunks).length
+				? [module, suppressed ].concat( sortArray(ret))
+				: [module ].concat( sortArray(ret));
+		},
+
+		resolveId: function resolveId(id) {
+			if (id === module) { return module; }
+			if (id === suppressed) { return suppressed; }
+			return null;
+		},
+
+		load: async function load(id) {
+			if (id === module) {
+				if (!imports.length) { return Promise.resolve(''); }
+				return Promise.resolve(options.exports
+					? imports.map(exporter).join('\n')
+					: imports.map(importer).join('\n'));
+			} else if (id === suppressed) {
+				if (!chunked.length) { return Promise.resolve(''); }
+				return Promise.resolve(chunked.map(exporter).join('\n'));
+			}
+		},
+
+		generateBundle: async function generateBundle(config, bundle) {
+			var fileName = (module.replace('\0', '_')) + ".js";
+			bundle[fileName].fileName = (module.replace('\0', '')) + ".js";
+			if (!imports.length) { delete bundle[fileName]; }
+			// I'm pretty sure this test is only valid when using
+			// rollup-plugin-iife
+			if (bundle[fileName] && /^\s*\(function\s*\(\)\s*\{\s*\}\)\(\);\s*$/g.test(bundle[fileName].code)) { delete bundle[fileName]; }
+			delete bundle[((suppressed.replace('\0', '_')) + ".js")];
+			// Get rid of empty bundles when using non-javascript files
+			// as entry points
+			for (var i = 0, list = Object.entries(bundle); i < list.length; i += 1) {
+				var ref = list[i];
+				var name = ref[0];
+				var chunk = ref[1];
+
+				if (imports.includes(chunk.facadeModuleId) && chunk.isEntry) {
+					delete bundle[name];
+				}
+			}
+		},
+	};
+}
+
+module.exports = entryGlob;
 //# sourceMappingURL=index.js.map
